a934eddc6f0ed090459118ebd3906b2c0dedb9d1
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-01 18:41:13 +0900

    feat: security 기본 설정

---
+++ src/main/java/hello/myblog/global/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,31 @@
+package hello.myblog.global.config;
+
+import hello.myblog.global.security.principal.CustomUserDetails;
+import java.util.Collection;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+@Configuration
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(authentication -> {
+                    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                    boolean isUser = authorities.contains(new SimpleGrantedAuthority("USER"));
+                    if (isUser) {
+                        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+                        return principal.getUsername();
+                    }
+                    return null;
+                });
+    }
+}
