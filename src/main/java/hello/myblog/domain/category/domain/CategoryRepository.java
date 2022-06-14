f5113133db51eb6958a099e3bd0b38762d14a62f
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-14 16:36:17 +0900

    카테고리 도메인 구현

---
+++ src/main/java/hello/myblog/domain/category/domain/CategoryRepository.java
@@ -0,0 +1,7 @@
+package hello.myblog.domain.category.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
