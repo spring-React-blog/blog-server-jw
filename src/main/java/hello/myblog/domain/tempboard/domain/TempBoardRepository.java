62a078bf3cc37c7f866aa55c009d034cc44d5dde
kimjinwook1 <rlawlsdnr628@naver.com>
2022-06-14 16:36:45 +0900

    임시게시판 도메인 구현

---
+++ src/main/java/hello/myblog/domain/tempboard/domain/TempBoardRepository.java
@@ -0,0 +1,7 @@
+package hello.myblog.domain.tempboard.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TempBoardRepository extends JpaRepository<TempBoard, Long> {
+
+}
